423dbcee9d23d649354d45507e5747a2e77cc6d7
FunnyBear38 <73970163+FunnyBear38@users.noreply.github.com>
2021-11-09 14:45:07 -0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,20 @@
+# This is a comment.
+# Functions Explanation
+
+# This is a built in python function:
+print("Hello World")
+# The only reason why this function runs is because we called it and we gave it data for it to do its purpose, which is to display text to the user on the screen.
+
+# We can create our own functions in Python
+#The def keyword is how we define a function in Python
+#in the line of code above, what comes after def is the name of our function, which in this case is main, and the parenthesis are for arguments
+# arguments are how we can give our function some info
+# we can literally have 500 arguments; we just have to make sure that we seperate our arguments w/ commas. We won't be using arguments in this file
+def main():
+  # indentation levels matter so, so, so, so, so, SO, much in Python. Why?
+    # Well, indentation levels and whitespace helps Python know what code belongs where.
+    #our print statement is indented underneath the main function because it is part of the function. if it weren't indented underneath the function, it would not be part of the function.
+  print("Hello Beautiful World")
+
+# We must call our function. If we don't call it, it won't run.
+main()
